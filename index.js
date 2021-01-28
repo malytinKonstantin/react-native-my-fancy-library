b677a85957d765510e5bf5cd78e4775d173d98f4
18516797 <KVMalyutin@sberbank.ru>
2021-01-28 18:57:37 +0300

    fix

---
+++ index.js
@@ -0,0 +1,11 @@
+import Icon from 'react-native-vector-icons';
+import React from 'react'
+import { View } from 'react-native'
+
+export default () => {
+    return (
+        <View>
+            <Icon name="rocket" size={30} color="#900" />
+        </View>
+    )
+}
